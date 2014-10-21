404f0f65ec39160cb739b04f3fd4b17dc9c3903d
Mark Wilkinson <mark@markwilkinson.me>
2014-10-21 18:37:19 +0100

    Add Section for Local Config Stuff
    e.g. uploads by proxy defines

--- local-config-sample.php
+++ local-config-sample.php
@@ -9,3 +9,10 @@ define( 'DB_NAME', 'wpskelton' );
 define( 'DB_USER', 'root' );
 define( 'DB_PASSWORD', 'root' );
 define( 'DB_HOST', 'localhost' ); // Probably 'localhost'
+
+/*******************************************************
+* You can add local specific things here
+* for example using the uploads-by-proxy plugin to load
+* media from uploads from the live site.
+*******************************************************/
+define( 'UBP_SITEURL', 'http://livedomain.com' );
