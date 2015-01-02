73d0a687b441255da10d9ef571667ee5d3876d81
Mark Wilkinson <mark@markwilkinson.me>
2015-01-02 20:14:04 +0000

    Add Custom Meta Boxes as a MU Plugin

---
+++ content/mu-plugins/custom-meta-boxes-loader.php
@@ -0,0 +1,12 @@
+<?php
+/*
+Plugin Name: Custom Meta Boxes
+Plugin URI: https://github.com/humanmade/Custom-Meta-Boxes
+Description: Lets you easily create metaboxes with custom fields that will blow your mind. Originally a fork of https://github.com/jaredatch/Custom-Metaboxes-and-Fields-for-WordPress.
+Version: 1.0.2
+License: GPL-2.0+
+Author: Human Made Limited
+Author URI: http://hmn.md
+*/
+
+require WPMU_PLUGIN_DIR.'/custom-meta-boxes/custom-meta-boxes.php';
---
+++ content/mu-plugins/plugin-control-loader.php
@@ -0,0 +1,10 @@
+<?php
+/*
+Plugin Name: Plugin Control
+Description: Allows you to control which plugins can be activated on local, staging and live.
+Version: 1.0
+Author: Mark Wilkinson
+Author URI: http://markwilkinson.me
+*/
+
+require WPMU_PLUGIN_DIR.'/plugin-control/plugin-control.php';
